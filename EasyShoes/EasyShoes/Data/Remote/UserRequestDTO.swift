7ab7417cfa2d2e93f5a81ea40e6d524cb05d286f
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-09 14:12:37 -0500

    feat: add models for User & UserRequestDTO

---
+++ EasyShoes/EasyShoes/Data/Remote/UserRequestDTO.swift
@@ -0,0 +1,11 @@
+//
+//  UserRequestDTO.swift
+//  EasyShoes
+//
+//  Created by Alumno on 9/06/25.
+//
+
+struct UserRequestDTO: Encodable {
+    let username: String
+    let password: String
+}
---
+++ EasyShoes/EasyShoes/Domain/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  EasyShoes
+//
+//  Created by Alumno on 9/06/25.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let username: String
+    let name: String
+    let image: String
+    let email: String
+}
