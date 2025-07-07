b5a858e064b13c2ec78e826119f6001aef47578c
jmayta1984 <jmayta@pucp.edu.pe>
2025-07-07 15:57:46 -0500

    chore: initial project setup

---
+++ EasyMovie/EasyMovie/Domain/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  EasyMovie
+//
+//  Created by Alumno on 7/07/25.
+//
+
+struct Movie: Identifiable {
+    let id: Int
+    let title: String
+    let poster: String
+    let overview: String
+}
